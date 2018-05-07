7bdf16e7787e8cfa2a4dfd3b15a7cc33340e3b16
Ataur Rahman Sayeed <rahmansayeed28@gmail.com>
2018-05-07 13:51:17 +0600

    Update bot.php

--- bot.php
+++ bot.php
@@ -3,27 +3,49 @@
  * Created by PhpStorm.
  * User: Sayeed
  * Date: 07-May-18
- * Time: 12:50 AM
+ * Time: 1:07 PM
  */
+$access_token = "EAAPKH1OtrVEBAFzmqxmSbdtnOZBEIEYODQ4x83gBQJy2SNoxyPP7qUDJc1GWQkMzZAn5Bf1rj7z7lmTXehakjFVCET3dAKZALu6AFBbIcGXA84w1bsO1GXlQFCbKtZB5ft5j9Lc6TOtvq3xfAsuCLeWc82QScFXRYduGFvB60wZDZD
+";
 
-include 'FbBot.php';
+$verify_token = "waytohappy";
 
-$tokken = $_REQUEST['hub_verify_token'];
-$hubVerifyToken = 'cloudwaysschool';
-$challange = $_REQUEST['hub_challenge'];
-$accessToken = 'EAAPKH1OtrVEBAKn4KZBG4HFGZCJMc2qpQv5brH0p8e8DZBFuTmCCY7tQbONt9jZCvIBNPrP4u48OgJ6I0p04htobJZCKVQXXRxDZCOQh6n80MyEIxJZBzrGjqpuOmghQosY0zuqQvZCNnvMoNPYG2VJhObca4302RkY6dzMDRh8ZAZCQZDZD';
-$bot = new FbBot();
-$bot->setHubVerifyToken($hubVerifyToken);
-$bot->setaccessToken($accessToken);
-$bot->verifyTokken($tokken, $challange);
-$message = $bot->readMessage($input);
-$botmessage = $bot->sendMessage($message);
+$hub_verify_token = null;
 
-if($messageText == 'hi') {
-    $answer = "Hello! How may I help you today ";
-    $response = [
-        'recipient' => [ 'id' => $senderId ],
-        'message' => [ 'text' => $answer ],
-        'access_token' => $this->accessToken
-    ]
-    ;}
+if(isset($_REQUEST['hub_mode']) && $_REQUEST['hub_mode'] == 'subscribe'){
+    $challenge =  $_REQUEST['hub_challenge'];
+    $hub_verify_token = $_REQUEST[$hub_verify_token];
+    if($hub_verify_token === $verify_token){
+        header('HTTP/1.1 200 OK');
+        echo $challenge;
+        die;
+    }
+}
+
+$input = json_decode(file_get_contents('php://input'),true);
+
+$sender = $input['entry'][0]['messaging'][0]['sender']['id'];
+$message = isset($input['entry'][0]['messaging'][0]['message']['text'])? $input['entry'][0]['messaging'][0]['message']['text']:'';
+
+if ($message){
+    $message_to_reply = 'This is the message to send back to the user';
+
+    $url = "https:graph.facebook.com/v2.6/me/messages?access_token=".$access_token;
+
+    $jsonData='{
+                    "recipient":{
+                            "id":'.$sender.'"
+                    },
+                    "message"{
+                            "text":"'.$message_to_reply.'"
+                    }
+                }';
+
+    $ch = curl_init($url);
+    curl_setopt($ch,CURLOPT_POST,1);
+    curl_setopt($ch,CURLOPT_POSTFIELDS,$jsonData);
+    curl_setopt($ch,CURLOPT_HTTPHEADER,array('Content-Type: application/json'));
+    curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,false);
+    $result = curl_exec($ch);
+    curl_close($ch);
+}
